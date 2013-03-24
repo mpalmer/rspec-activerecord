d5c0bf86fec07a8a06e03ce681a740de71e775ce
Matt Palmer <mpalmer@hezmatt.org>
2013-03-25 08:30:57 +1100

    Support setting pre_loaded_fixtures, too

--- lib/rspec/active_record.rb
+++ lib/rspec/active_record.rb
@@ -12,6 +12,7 @@ module RSpec
 				self.fixture_path = RSpec.configuration.fixture_path
 				self.use_transactional_fixtures = RSpec.configuration.use_transactional_fixtures
 				self.use_instantiated_fixtures  = RSpec.configuration.use_instantiated_fixtures
+				self.pre_loaded_fixtures        = RSpec.configuration.pre_loaded_fixtures
 				fixtures RSpec.configuration.global_fixtures if RSpec.configuration.global_fixtures
 			end
 		end
@@ -21,6 +22,7 @@ module RSpec
 			
 			c.add_setting :use_transactional_fixtures, :alias_with => :use_transactional_examples
 			c.add_setting :use_instantiated_fixtures
+			c.add_setting :pre_loaded_fixtures
 			c.add_setting :global_fixtures
 			c.add_setting :fixture_path
 		end
